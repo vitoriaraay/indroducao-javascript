fe52ac502a9fc2a07d21bc2c81038b33be7f0549
vitoriaraay <vitoria.lobo.melo@escola.pr.gov.br>
2025-11-05 11:41:58 -0300

    js principal

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,17 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 /* IMC = massa/altura^2  */
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso")
+var peso = tdPeso.textContent;
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var imc = peso / (altura*altura);
+
+console.log(paciente);//tr
+console.log(tdPeso);// td que tem peso
+console.log(tdAltura); // td que tem altura
+console.log(peso); // valor do peso
+console.log(altura); // valor da altura
+
+console.log(imc)
