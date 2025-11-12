9a14ad080a1f93a70da9abde4803359ef66a5750
vitoriaraay <vitoria.lobo.melo@escola.pr.gov.br>
2025-11-12 10:26:28 -0300

    correção

--- js/principal.js
+++ js/principal.js
@@ -1,18 +1,44 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-/* IMC = massa/altura^2  */
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = document.querySelector(".info-peso")
-var peso = tdPeso.textContent;
-var tdAltura = document.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-var imc = peso / (altura*altura);
+/*
+console.log(paciente);  //tr
+console.log(tdPeso);    // td que tem peso
+console.log(tdAltura);  //td que tem altura
+console.log(peso);      //valor do peso
+console.log(altura);    //valor da altura
+*/
 
-console.log(paciente);//tr
-console.log(tdPeso);// td que tem peso
-console.log(tdAltura); // td que tem altura
-console.log(peso); // valor do peso
-console.log(altura); // valor da altura
+var pacientes = document.querySelectorAll(".paciente");
 
-console.log(imc)
+for (var i=0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];      // inserir
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    //validar os dados de peso
+    if (peso <= 0 ||  peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+    //validar a altura
+    if (altura <=0 || altura > 3.00){
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+    }
+    //validar peso e altura
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+}
